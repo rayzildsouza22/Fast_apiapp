f4cae851eed5b805811c3cba9d6ad7eeeefe2ed6
Rayzil DSouza <rayzilledsouza@gmail.com>
2026-07-01 22:22:41 +1100

    auth

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -31,3 +31,12 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     return db_user
+
+@router.post("/login",response_model=UserResponse)
+def login(user: UserCreate, db:Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if not existing_user:
+        raise HTTPException(status_code=404,detail="User not found")
+    if not verify_password(user.password, existing_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+    return existing_user
